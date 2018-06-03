50e35f4ea268240b4ea8b02b01d3803161adba8f
Pablo Leban <pablo@leban.com.ar>
2018-06-03 02:24:35 -0300

    Only bring transactions on valid date

--- socket_api/index.js
+++ socket_api/index.js
@@ -15,19 +15,21 @@ setInterval(async () => {
     const rawMatches = await matchService.getMatches();
     let emit = false;
     const matches = await rawMatches.reduce(async (p, m) => {
-      const transactions = await etherscan.getTransactions(m);
-      if (
-        transactions.team1.length > m.team1.transactions.length ||
-        transactions.team2.length > m.team2.transactions.length
-      ) {
-        m.team1.transactions = transactions.team1;
-        m.team2.transactions = transactions.team2;
-        emit = true;
+      if(new Date(m.date) > new Date()){
+        const transactions = await etherscan.getTransactions(m);
+        if (
+          transactions.team1.length > m.team1.transactions.length ||
+          transactions.team2.length > m.team2.transactions.length
+        ) {
+          m.team1.transactions = transactions.team1;
+          m.team2.transactions = transactions.team2;
+          emit = true;
+        }
+        const savedMatch = await m.save();
+        await helpers.delay(250);
+        const resolvedMatches = await p;
+        return [...resolvedMatches, savedMatch];
       }
-      const savedMatch = await m.save();
-      await helpers.delay(250);
-      const resolvedMatches = await p;
-      return [...resolvedMatches, savedMatch];
     },[]);
     if (emit) io.emit('all-matches', matchService.matchesCalculations(matches));
   } catch (err) {
