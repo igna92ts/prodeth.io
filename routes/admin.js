eb8dd0fa6d2b8772e70bf6c51ca0e42d14d1e5fb
Ignacio Torres <igna_torres92@hotmail.com>
2018-06-06 22:04:56 -0300

    added delete method

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,25 @@ const express = require('express'),
   errors = require('../errors');
 
 /* GET home page. */
-router.post('/', async (req, res, next) => {
+router.post('/match', async (req, res, next) => {
   const { countryCode1, countryCode2, date, timezone } = req.body;
   if (countryCode1 && countryCode2 && date && timezone) {
     try {
       const result = await matchService.createMatch(countryCode1, countryCode2, date, timezone);
+      res.send(result);
+    } catch (err) {
+      res.send(errors.badRequest(err));
+    }
+  } else {
+    res.send(errors.badRequest('Wrong parameters'));
+  }
+});
+
+router.delete('/match', async (req, res, next) => {
+  const { countryCode1, countryCode2, date, timezone } = req.query;
+  if (countryCode1 && countryCode2 && date && timezone) {
+    try {
+      const result = await matchService.deleteMatch(countryCode1, countryCode2, date, timezone);
       res.send(result);
     } catch (err) {
       res.send(errors.badRequest(err));
