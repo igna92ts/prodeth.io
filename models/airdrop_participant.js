2433fd8d0f5ba311b3c42c47981b5412fad3dd95
Ignacio Torres <igna_torres92@hotmail.com>
2018-06-10 22:42:52 -0300

    Fixed registered validation

--- models/airdrop_participant.js
+++ models/airdrop_participant.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const airdropParticipants = mongoose.Schema({
-  address: { type: String, required: true },
+  address: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   verificationHash: { type: String, required: true, unique: true },
   verified: { type: Boolean, required: true, default: false }
